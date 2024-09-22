60824c9ee1d00f62341eadffaf645e33e7d6b364
Gleb Laychenkov <racers2no95@mail.ru>
2024-09-22 10:35:01 +0400

    Initial commit 2

---
+++ cypress.env.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    "viewportWidth": 1920,
+    "viewportHeight": 1080,
+    baseUrl: "http://localhost:3000/",
+    retries: 2
+    },
+  },
+);
